28046c7f102c4afd88ba175e7e6c03dfaceb92d5
Emanuel Pimentel <ep3100@columbia.edu>
2025-08-17 13:48:07 -0400

    edit made for pull request

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
-
+# 2025 edit for application
 scoreboard = [
     {
     "id": 1,
